ddf3a1f110c95fe8278298e5734e772c40b454e6
baracat <joaodobingo@gmail.com>
2021-04-25 16:00:41 -0300

    Add project files.

---
+++ RealEstateManager.API/Controllers/GraphQLController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+using GraphQL;
+using RealEstateManager.Utilities;
+
+namespace RealEstateManager.API.Controllers
+{
+    [Route("[controller]")]
+    //[ApiController]
+    public class GraphQLController : Controller
+    {
+        public GraphQLController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            if (query == null)
+            {
+                throw new ArgumentException(nameof(query));
+            }
+
+            var inputs = query.Variables?.ToInputs();
+
+            //return Ok();
+        }
+    }
+}
